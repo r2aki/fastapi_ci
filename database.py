e5d788c4bc225c223e2331cf6e404c01a970b69b
r2aki <so.nice.dk.ir@gmail.com>
2025-10-16 20:16:50 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.pool import NullPool  # ⬅️ добавили
 
 DATABASE_URL = "sqlite+aiosqlite:///./recipes.db"
 
@@ -8,6 +9,8 @@ engine = create_async_engine(
     DATABASE_URL,
     echo=False,
     future=True,
+    poolclass=NullPool,
+    connect_args={"timeout": 30},
 )
 SessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
